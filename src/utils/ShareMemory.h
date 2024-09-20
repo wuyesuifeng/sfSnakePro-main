071aaa9c82ddd9b660e3f52e11279e3260cc4442
xy <xingyi137@gmail.com>
2024-09-20 23:45:52 +0800

    修复bug

--- src/utils/ShareMemory.h
+++ src/utils/ShareMemory.h
@@ -5,9 +5,9 @@
 
 #define READ_P_LEN 4
 #define READ_LEN 16 + 1
-#define VISION_HARM_POS 802
-#define VISION_CHECK_POS 1604
-#define WRITE_LEN 2408 + 1
+#define VISION_CHECK_POS 802
+#define VISION_HARM_POS 1602
+#define WRITE_LEN 2402 + 1
 
 namespace utils {
     class ShareMemory {
